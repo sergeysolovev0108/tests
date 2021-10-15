6be98064ed0e7828da5dd49cf534a8a9a7680534
Sergey <61157343+sergeysolovev0108@users.noreply.github.com>
2021-10-15 21:27:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log('Loading function');
 var sign = require('./index')
 const fs = require("fs");
-require('dotenv').config();
+require('dotenv').config({path: __dirname + '/.env'});
 const aws = require('aws-sdk');
 const result = require('dotenv').config();
 
